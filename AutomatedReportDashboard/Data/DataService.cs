ff655c508a34a90a3f0729dbba0eaec2f61e0c8e
Adel-Al-AlSbaey <adelalsbaey.999@gmail.com>
2024-03-14 15:07:15 +0200

    DivisinManager

--- AutomatedReportDashboard/Data/DataService.cs
+++ AutomatedReportDashboard/Data/DataService.cs
@@ -23,9 +23,33 @@ namespace AutomatedReportDashboard.Data
             GeneralResponse Data = JsonSerializer.Deserialize<GeneralResponse>(result, options);
             return Data;
         }
-        public async Task<GeneralResponse> Login(string UserType, string Password)
+        public async Task<GeneralResponse> Login(string UserId, string Password)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, baseUrl + $"api/User/Login?Id={UserType}&Password={Password}");
+            var request = new HttpRequestMessage(HttpMethod.Get, baseUrl + $"api/User/Login?Id={UserId}&Password={Password}");
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            GeneralResponse Data = JsonSerializer.Deserialize<GeneralResponse>(result, options);
+            return Data;
+        }
+        public async Task<GeneralResponse> EditePassword(string UserId, string Password)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Put, baseUrl + $"api/User/EditeUserPassword?Id={UserId}&NewPassword={Password}");
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            GeneralResponse Data = JsonSerializer.Deserialize<GeneralResponse>(result, options);
+            return Data;
+        }
+        public async Task<GeneralResponse> GetAllCertificates()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, baseUrl + "api/Certificate/GetAllCertificates");
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            GeneralResponse Data = JsonSerializer.Deserialize<GeneralResponse>(result, options);
+            return Data;
+        }
+        public async Task<GeneralResponse> GetAllDivisionsWithStudentsNomber(Guid Id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, baseUrl+$"api/Division/GetAllDivisionsWithStudentsNomber?certificateId={Id}");
             var response = await client.SendAsync(request);
             var result = await response.Content.ReadAsStringAsync();
             GeneralResponse Data = JsonSerializer.Deserialize<GeneralResponse>(result, options);
--- AutomatedReportDashboard/Data/Helper.cs
+++ AutomatedReportDashboard/Data/Helper.cs
@@ -5,6 +5,7 @@ namespace AutomatedReportDashboard.Data
     public static class Helper
     {
         public static Guid UserId { get; set; } /*= new Guid("10f3562f-1a07-4dab-a16b-8b64855df56d");*/
+        public static Guid CertificateId { get; set; }
         public static T ToObj<T>(this object data)
         {
             var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
