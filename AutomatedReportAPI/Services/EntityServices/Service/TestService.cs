bf20e6c2609ef90b432121ffe9f360538761c301
mohammad ramadan <aboabdallahramadan@gmail.com>
2024-09-07 11:24:12 +0300

    fixing-edit-test-and-is-done-value

--- AutomatedReportAPI/Services/EntityServices/Service/TestService.cs
+++ AutomatedReportAPI/Services/EntityServices/Service/TestService.cs
@@ -122,6 +122,7 @@ namespace AutomatedReportAPI.Services.EntityServices.Service
                     Id = requste.Id,
                     DateTime = requste.DateTime,
                     Description = requste.Description,
+                    IsDone = requste.IsDone,
                     TotalMark = requste.TotalMark,
                     DivisionId = division.Id,
                     SubjectId = subject.Id
